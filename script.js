0de93b1d7caa0b6e49cacfb32d434e782477bafb
plantbasedmeats <102681486+plantbasedmeats@users.noreply.github.com>
2022-03-31 15:24:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const links = ["https://i.imgur.com/DaajwK9.jpg", "https://i.imgur.com/VrcOlpL.jpg", "https://i.imgur.com/pRTdxLj.jpg", "https://i.imgur.com/pWJGhn9.jpg", "https://i.imgur.com/2sLEQfJ.jpg"]
+const links = ["https://i.imgur.com/DaajwK9.jpg", "https://i.imgur.com/VrcOlpL.jpg", "https://i.imgur.com/pRTdxLj.jpg", "https://i.imgur.com/pWJGhn9.jpg", "https://i.imgur.com/2sLEQfJ.jpg", "https://i.imgur.com/Tjy3ObO.jpg", "https://i.imgur.com/NN5CLFf.jpg", "https://i.imgur.com/FiQFIU6.jpg", "https://i.imgur.com/fpflUTP.jpg", "https://i.imgur.com/z1qEVsH.jpg"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
