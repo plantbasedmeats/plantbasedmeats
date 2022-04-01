9cf551e57ce780d854075eb19e9389d7e635e575
plantbasedmeats <102681486+plantbasedmeats@users.noreply.github.com>
2022-04-01 09:54:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const links = ["https://i.imgur.com/DaajwK9.jpg", "https://i.imgur.com/VrcOlpL.jpg", "https://i.imgur.com/pRTdxLj.jpg", "https://i.imgur.com/pWJGhn9.jpg", "https://i.imgur.com/2sLEQfJ.jpg", "https://i.imgur.com/Tjy3ObO.jpg", "https://i.imgur.com/NN5CLFf.jpg", "https://i.imgur.com/FiQFIU6.jpg", "https://i.imgur.com/fpflUTP.jpg", "https://i.imgur.com/z1qEVsH.jpg"]
+const links = ["https://i.imgur.com/iYLwFMI.jpg", "https://i.imgur.com/XOtO8QM.png", "https://i.pinimg.com/550x/26/5d/0f/265d0f98787e4588434bf7140e68e038.jpg", "https://i.imgur.com/DaajwK9.jpg", "https://i.imgur.com/VrcOlpL.jpg", "https://i.imgur.com/pRTdxLj.jpg", "https://i.imgur.com/pWJGhn9.jpg", "https://i.imgur.com/2sLEQfJ.jpg", "https://i.imgur.com/Tjy3ObO.jpg", "https://i.imgur.com/NN5CLFf.jpg", "https://i.imgur.com/FiQFIU6.jpg", "https://i.imgur.com/fpflUTP.jpg", "https://i.imgur.com/z1qEVsH.jpg"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
